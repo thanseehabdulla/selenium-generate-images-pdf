0bc209e0a040595980b4f7044aaee74157ef922d
abdullathanseeh <abdullathanseeh@simelabs.com>
2019-04-08 16:57:15 +0530

    intial

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const imagesToPdf = require("images-to-pdf")
+// const imagesToPdf = require("images-to-pdf")
+const PDFDocument = require('pdfkit');
+let fs = require('fs');
 
 console.log("please wait..we are generating pdf");
 
-imagesToPdf(["test2.png", "test4.png", "test5.png", "test6.png", "test7.png", "test8.png", "test9.png", "test10.png", "test11.png"], "final2.pdf")
+const doc = new PDFDocument;
+
+doc.pipe(fs.createWriteStream('output.pdf'));
+
+let n = 11
+
+for(let i = 2; i<=n;i++) {
+    if (i !== 3) {
+
+        doc.image('test' + i + '.png', {
+            fit: [500, 500],
+            align: 'center',
+            valign: 'center'
+        });
+        if(i !== n)
+        doc.addPage()
+    }
+}
+
+doc.end();
+
+// imagesToPdf(["test2.png", "test4.png", "test5.png", "test6.png", "test7.png", "test8.png", "test9.png", "test10.png", "test11.png"], "final2.pdf")
 
