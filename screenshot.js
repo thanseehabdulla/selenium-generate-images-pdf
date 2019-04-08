b920438a766b85e1ac2fc61169bc0951ccd40144
abdullathanseeh <abdullathanseeh@simelabs.com>
2019-04-08 18:24:37 +0530

    json reading

--- screenshot.js
+++ screenshot.js
@@ -8,7 +8,7 @@ function timeout(ms) {
 (async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({width: 5960, height: 14209})
+    await page.setViewport({width: 5960, height: 14209});
     // await page.goto('http://stackoverflow.com', {waitUntil: 'networkidle2'});
     await page.goto("file:///home/sinergia/Desktop/davis/selenium/new.html");
 
@@ -17,7 +17,7 @@ function timeout(ms) {
 
 
 
-    await imagesToPdf(["screenshot.png"], "screenshot.pdf")
+    await imagesToPdf(["screenshot.png"], "screenshot.pdf");
     browser.close();
 
 })();
